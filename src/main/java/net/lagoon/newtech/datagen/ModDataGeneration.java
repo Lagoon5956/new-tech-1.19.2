c668e8c513de51942c7d12ac37625a520c9b2305
Lagoon5956 <stevendevaney1503@gmail.com>
2025-09-02 13:21:33 +0100

    added data gen

---
+++ src/main/java/net/lagoon/newtech/datagen/ModDataGeneration.java
@@ -0,0 +1,13 @@
+package net.lagoon.newtech.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModDataGeneration implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        fabricDataGenerator.addProvider(ModModelProvider::new);
+        fabricDataGenerator.addProvider(ModRecipeGenorator::new);
+        fabricDataGenerator.addProvider(ModLootTableGenerator::new);
+    }
+}
