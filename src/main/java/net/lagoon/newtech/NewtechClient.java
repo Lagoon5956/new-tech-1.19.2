e7b067ff12e7cdb6922f197b8c0687a47ba2b57b
Lagoon5956 <stevendevaney1503@gmail.com>
2025-09-02 12:31:32 +0100

    The begining... and a lot of pain

---
+++ src/main/java/net/lagoon/newtech/NewtechClient.java
@@ -0,0 +1,10 @@
+package net.lagoon.newtech;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NewtechClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
